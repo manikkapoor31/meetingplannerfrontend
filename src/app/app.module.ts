d3d0d0577fc9b9f2c8e09c4aed17e4f184521616
Manik Kapoor <mkapoor765@gmail.com>
2020-06-03 05:11:31 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { RouterModule,ActivatedRoute,ParamMap} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'*',component:LoginComponent},
+      {path:'**',component:LoginComponent},
+      {path:'login',component:LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
